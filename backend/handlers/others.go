2414761d7d31f208ea9ecd42aeb4eb00450f4d96
yueli <ubuntu@yuelili.com>
2024-06-17 20:03:55 +0800

    修复一些bug

--- backend/handlers/others.go
+++ backend/handlers/others.go
@@ -4,6 +4,7 @@ import (
 	"docsfly/global"
 	"docsfly/models"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -38,4 +39,56 @@ func GetRndPoem(c *gin.Context) {
 func GetAppVersion(c *gin.Context) {
 	sendSuccessResponse(c, time.Now(), global.AppConfig.AppVersion)
 
+}
+
+type VisitorInfo struct {
+	IP string
+}
+
+func VisitorInsertLog(c *gin.Context) {
+
+	clientTime := currentTime()
+	url := c.Query("url")
+	dbContext, exists := c.Get("db")
+	if !exists {
+		return
+	}
+	db := dbContext.(*gorm.DB)
+
+	var count int64
+	db.Scopes(BasicModel, MatchUrlPath(url)).Count(&count)
+
+	if count == 0 {
+		sendErrorResponse(c, http.StatusInternalServerError, clientTime, "Can't find target link")
+		return
+	}
+
+	urlList := strings.Split(url, "/")
+
+	var category, book, locale string
+
+	if len(urlList) > 2 {
+		category = urlList[0]
+		book = urlList[1]
+		locale = urlList[2]
+	} else {
+		sendErrorResponse(c, http.StatusInternalServerError, clientTime, "Can't find target path")
+		return
+	}
+
+	today := time.Now().Local()
+
+	vs := models.Visitor{
+		IP:       c.ClientIP(),
+		URL:      url,
+		Time:     today,
+		Category: category,
+		Book:     book,
+		Locale:   locale,
+	}
+
+	db.Model(&models.Visitor{}).Create(&vs)
+
+	// 返回 IP 地址给客户端
+	sendSuccessResponse(c, clientTime, gin.H{"message": "success"})
 }
--- backend/routes/cors.go
+++ backend/routes/cors.go
@@ -8,7 +8,7 @@ import (
 func registerCors(engine *gin.Engine) {
 	config := cors.DefaultConfig()
 	// 指定允许访问的源
-	config.AllowOrigins = []string{"http://localhost:5173"}
+	config.AllowOrigins = []string{"http://localhost:5173", "https://docs.yuelili.com"}
 	config.AllowHeaders = []string{"Origin", "Authorization", "Content-Type", "Content-Length"}
 	// 允许携带凭证（例如 Cookies）
 	config.AllowCredentials = true
